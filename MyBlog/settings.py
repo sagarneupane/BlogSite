1f9c33802012f636d77fcc8cc78a7a0a75655894
sagar <cyber.crime252@gmail.com>
2022-05-12 08:10:53 +0545

    Kati Commit Garna pareko

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [BASE_DIR, 'static']
+STATIC_ROOT = [STATIC_DIR, c]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
