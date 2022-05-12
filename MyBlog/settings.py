ae91f2cf447bd80c19bd3910e9170bb827ae86bf
sagar <cyber.crime252@gmail.com>
2022-05-12 08:22:33 +0545

    Kati Commit Garna pareko4

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [STATIC_DIR,]
+STATIC_ROOT = [BASE_DIR, 'staticfiles']
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
